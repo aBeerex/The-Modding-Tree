9b03c5c48d2efd34c934c5d0d86b9b273a3fc870
aBeerex <136271474+aBeerex@users.noreply.github.com>
2023-06-11 19:12:37 -0500

    achievements
    added achievements layer and 1 achievement

--- js/layers.js
+++ js/layers.js
@@ -2,6 +2,7 @@ addLayer("a", {
     name: "A", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "A", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    row: 0, // Row the layer is in on the tree (0 is the first row)
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
@@ -20,9 +21,28 @@ addLayer("a", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
+    layerShown(){return true}
+})
+
+addLayer("achiev", {
+    name: "Achievements",
+    symbol: "✦",
+    tooltip: "Achievements",
+    position: 0,
+    row: "side",
+    startData() { return {
+	    unlocked: true,
+    }}
+    achievements: {
+	11: {
+		name: "Hey, You're Finally Awake",
+		tooltip: "Start playing the game for real by resetting for A",
+		icon: "https://i.kym-cdn.com/entries/icons/original/000/020/143/squidward.jpg",
+		done() { return player.a.A.gte(1)}
+	}
+}
     layerShown(){return true}
 })
